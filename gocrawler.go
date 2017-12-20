551c3d76b1a40858cbc51a934ca2ab13fd14fc94
Keanan Koppenhaver <keanan@howtogeek.com>
2017-12-20 02:52:43 -0600

    Allow filepath to be passed in from the command line, write root URL page to passed in path

--- gocrawler.go
+++ gocrawler.go
@@ -5,18 +5,35 @@ import (
 	"strings"
 	"os"
 	"net/http"
-	"io"
 	"flag"
 	"bytes"
+	"io/ioutil"
+	"path/filepath"
 )
+	
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 // Inspired by: https://github.com/thbar/golang-playground/blob/master/download-files.go
-func fetchURL( url string ) {
+func fetchURL( url string, rootURL string, pathToWrite string ) {
 	tokens := strings.Split( url, "/" )
 	fileName := tokens[len( tokens ) - 1]
 	fmt.Println( "Downloading", url, "to", fileName )
 
-	output, err := os.Create( fileName )
+	var pathBuffer bytes.Buffer
+
+	pathBuffer.WriteString(pathToWrite)
+	pathBuffer.WriteString("/")
+	pathBuffer.WriteString(fileName)
+
+	filePath := pathBuffer.String()
+
+	// If this URL is the root URL, the final filename should be index.html
+
+	output, err := os.Create( filePath )
 	if err != nil {
 		fmt.Println( "Error while creating", fileName, "-", err )
 		return
@@ -30,17 +47,16 @@ func fetchURL( url string ) {
     buf.ReadFrom( response.Body )
     contentsOfPage := buf.String()
 
+    strToWrite := []byte(contentsOfPage)
+
 	if err != nil {
 		fmt.Println( "Error while downloading", url, "-", err )
 		return
 	}
 	defer response.Body.Close()
 
-	_, error := io.Copy( output, response.Body )
-	if error != nil {
-		fmt.Println( "Error while downloading", url, "-", err )
-		return
-	}
+    writeError := ioutil.WriteFile( filePath, strToWrite, 0755)
+    check(writeError)
 
 	// Loop through each of the assets specified on the page and download them
 	// to the correct path based on the root directory    
@@ -60,13 +76,13 @@ func downloadAsset(assetPath string) {
 func selectLinks(pageContents string) {
 	// Parse the HTML to grab all links that match the root domain to recursively parse
 	// by adding them to the global link queue
-	fmt.Printf( pageContents )
 }
 
 func main() {
 	urlsToParse := make( []string, 0 )
 
-	urlPtr := flag.String( "url", "", "The base URL of the site to be downloaded. ex) http://google.com" )
+	urlPtr := flag.String( "url", "", "(required) The base URL of the site to be downloaded. ex) http://google.com" )
+	filePtr := flag.String( "path", "", "(required) The path where the downloaded site will be stored." )
 
 	flag.Parse()
 
@@ -76,16 +92,22 @@ func main() {
 		return
 	}
 
+	// If a file path was not specified, throw an error
+	if *filePtr == "" {
+		fmt.Println( "The path param must not be empty. Provide a path with the --path flag." )
+		return
+	}
+
 	// Set up the root for the parse, based on the provided directory
 	urlsToParse = append( urlsToParse, *urlPtr )
 
-	// Create a folder named after the domain that was passed in
+	// Create a folder based on the path passed in as a command line arg
+	os.Mkdir( *filePtr, 0755)
+	pathToWrite, _ := filepath.Abs(*filePtr)
 
 	// If present, parse a new link from the queue every 5 seconds
 
 	// If queue is empty for 30 seconds, end parsing
 
-	fetchURL(urlsToParse[0])
-	
-	fmt.Println( urlsToParse )
+	fetchURL( urlsToParse[0], *urlPtr, pathToWrite )
 }
